7daec671339f7919e2082aa2249ca0074b49f914
Patryk <patryk.memo@gmail.com>
2021-05-25 19:02:18 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,215 @@
+# React hook use-form-schema
+
+`use-form-schema` implements React hook to handle form validation and errors messages.
+
+## Installation
+
+Use your package manager:
+
+`npm i use-form-schema`
+
+or
+
+`yarn add use-form-schema`
+
+## Usage
+
+First you have to import hook:
+
+```jsx
+import useFormSchema from 'use-form-schema';
+```
+
+Next, define your form schema for validation process and create function for handling form data:
+
+```jsx
+const AppLoginForm = () => {
+    const [errorMessage, setErrorMessage] = useState('');
+    const schema = {
+        login: {
+            required: 'Field is required',
+            min: [4, 'Login must contain at least 4 characters']
+        },
+        password: {
+            required: 'Field is required'
+        }
+    }
+    
+	const { register, errors, setErrors, isValidated } = useFormSchema(schema);
+    const handleSubmit = data => {
+        ... if(req.status === 401) setErrorMessage('Bad credentials') ...
+        // or to handle messages from your backend
+        ... if(req.status === 400) setErrors(err.response.data.messages) ...
+    }
+    
+    return (...);
+}
+```
+
+Form example:
+
+```jsx
+<form {...register(handleSubmit)}>
+    <input type="text" name="login"/>
+    { errors.login && <p>{errors.login[0]}</p>}
+
+    <input type="password" name="password"/>
+    { errors.password && <p>{errors.password[0]}</p>}
+
+    { errorMessage && isValidated && <p>{errorMessage}</p>}
+
+    <button type="submit">Log in</button>
+</form>
+```
+
+## Documentation
+
+`use-form-schema` hook takes the `schema` parameter and returns `register, setErrors, reset` functions, `errors` object and `isValidated` boolean.
+
+`const { register, errors, setErrors, isValidated, reset } = useFormSchema(schema);`
+
+**Schema** is an object in which you define what fields you want to validate. For example:
+
+```jsx
+const schema = {
+    login: {
+        required: 'Field is required!'
+    }
+}
+```
+
+Input with name `login` can't be empty, otherwise in `errors` object an appropriate message will be added. 
+
+To show the error message: *(use index zero to show one message)*
+
+```jsx
+{ errors.login && errors.login[0] }
+```
+
+**isValidated** tells when form was validated and sended. If you do any changes after submit, the value will be `false`. Used to show or hide messages after the current form has been submitted.
+
+**setErrors** function sets errors for each field. The param what that function takes must be an object like:
+
+```jsx
+{
+    fieldname: ['message1', 'message2'],
+    otherfieldname: ['message']
+}
+```
+
+Used to handle messages from API, for example if login or email are already taken.
+
+**register** function handles all form data by adding event listeners for each field. Takes `callback` parameter and if form is valid, passes an object with data to this callback. Callback is used for handling API requests with form data.
+
+```jsx
+<form {...register(handleSubmit)}>
+```
+
+**reset** function clears errors and fields.
+
+**NOTE! If you want to handle files add  `encType="multipart/form-data"` to form tag!**
+
+### Validators
+
+| Validator                                                    | Params                                  | Usage                                                        |
+| ------------------------------------------------------------ | --------------------------------------- | ------------------------------------------------------------ |
+| required                                                     | message:string                          | { required: 'Field is required!' }                           |
+| min                                                          | array[value:integer, message:string]    | { min: [4, 'Field text is too short'] }                      |
+| max                                                          | array[value:integer, message:string]    | { max: [12, 'Field text is too long'] }                      |
+| equalField, notEqualField, ltField, lteField, gtField, gteField | array[fieldName:string, message:string] | { equalField: ['password', 'Passwords is not the same'] }    |
+| equal, notEqual, lt, lte, gt, gte                            | array[value:any, message:string]        | { equal: [1000, 'Field value is not equal to 1000'] }        |
+| in                                                           | array[values:array, message:string]     | { in: [['banana', 'apple'], 'Field value is not valid fruit'] } |
+| notIn                                                        | array[values:array, message:string]     | { notIn: [['corn', 'leek'], 'Field value is fruit'] }        |
+| regexp                                                       | array[value:regexp, message:string]     | { regexp: [/^[0-9]+/, 'Field value is not integer'] }        |
+| pattern                                                      | array[value:string, message:string]     | { pattern: ['email', 'Is not valid email address'] }         |
+
+### Validators for handling files
+
+| Validator                                                 | Params                              | Usage                                                      |
+| --------------------------------------------------------- | ----------------------------------- | ---------------------------------------------------------- |
+| required                                                  | message:string                      | { required: 'Field is required!' }                         |
+| ext                                                       | array[value:string, message:string] | { ext: ['png', 'File must be png' }                        |
+| extAllowed                                                | array[value:string, message:string] | { extAllowed: [['png', 'jpg'], 'File is not allowed'] }    |
+| extNotAllowed                                             | array[value:string, message:string] | { extNotAllowed: [['zip', 'rar'], 'File is not allowed'] } |
+| sizeEqual, sizeNotEqual, sizeGt, sizeGte, sizeLt, sizeLte | array[value:any, message:string]    | { sizeLt: [2000, 'File is to big'] }                       |
+| type, typeIn, typeNotIn                                   | array[value:any, message:string]    | { type: ['text/plain', 'Unknow file type'] }               |
+
+### Custom validators
+
+| Validator | Params                                               | Usage                                                        |
+| --------- | ---------------------------------------------------- | ------------------------------------------------------------ |
+| validate  | array[value:function, params:object, message:string] | { validate: [checkPassword, { blackList }, 'Password is not allowed'] } |
+| validate  | array[value:function, message:string]                | { validate: [checkPassword, 'Password is the same as login'] } |
+| validator | value:function                                       | { validator: checkPassword }                                 |
+
+Examples for custom validators:
+
+**validate with params**
+
+```jsx
+const blackList = ['admin', 'password', 'test'];
+
+const checkPassword = (fieldValue, params, formData) => {
+    return !params.blackList.includes(fieldValue);
+}
+
+const schema = {
+    login: {
+      	required: 'Field is required',
+    },
+    password: {
+        required: 'Field is required',
+        validate: [checkPassword, { blackList }, 'Password is not allowed']
+    }
+};
+```
+
+**validate without params**
+
+```jsx
+const checkPassword = (fieldValue, formData) => {
+    return !(fieldValue === formData.login);
+}
+
+const schema = {
+    login: {
+      	required: 'Field is required',
+    },
+    password: {
+        required: 'Field is required',
+        validate: [checkPassword, 'Password is the same as login']
+    }
+};
+```
+
+**validator**
+
+```jsx
+const checkPassword = (fieldValue, formData) => {
+    if(fieldValue === formData.login) return 'Password is the same as login';
+    if(fieldValue.length < 3) return 'Password is too short!';
+}
+
+const schema = {
+    login: {
+      	required: 'Field is required',
+    },
+    password: {
+        required: 'Field is required',
+        validator: checkPassword
+    }
+};
+```
+
+### Patterns
+
+Pattern list for **pattern** validator:
+
+| Pattern | Description                                          |
+| ------- | ---------------------------------------------------- |
+| email   | Checks if field value is valid email address         |
+| number  | Check if field value is number                       |
+| double  | Check if field value is double (with dot ex: 12.551) |
+| integer | Check if field value is integer                      |
+| alpha   | Check if field value has alphanumeric characters     |
+
---
+++ index.js
@@ -0,0 +1,221 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+
+export default function useFormSchema(schema) {
+    const ref = useRef();
+    const [isValidated, setIsValidated] = useState(false);
+    const [errors, setErrors] = useState({});
+
+    const checkConstraint = useCallback((constraintName, fieldValue, constraintParams, data) => {
+        if (fieldValue.name !== undefined) {
+            // Handle file type inputs
+            switch (constraintName) {
+                case 'required':
+                    return !!fieldValue.name;
+                case 'ext':
+                    return fieldValue.name.split('.').pop() === constraintParams[0];
+                case 'extAllowed':
+                    return constraintParams[0].includes(fieldValue.name.split('.').pop());
+                case 'extNotAllowed':
+                    return !constraintParams[0].includes(fieldValue.name.split('.').pop());
+                case 'sizeEqual':
+                    return fieldValue.size === constraintParams[0];
+                case 'sizeNotEqual':
+                    return fieldValue.size !== constraintParams[0];
+                case 'sizeGt':
+                    return fieldValue.size > constraintParams[0];
+                case 'sizeGte':
+                    return fieldValue.size >= constraintParams[0];
+                case 'sizeLt':
+                    return fieldValue.size < constraintParams[0];
+                case 'sizeLte':
+                    return fieldValue.size <= constraintParams[0];
+                case 'type':
+                    return fieldValue.type === constraintParams[0];
+                case 'typeIn':
+                    return constraintParams[0].includes(fieldValue.type);
+                case 'typeNotIn':
+                    return !constraintParams[0].includes(fieldValue.type);
+                case 'validator':
+                    return constraintParams(fieldValue, data);
+                case 'validate':
+                    if (constraintParams.length === 3) {
+                        return constraintParams[0](fieldValue, constraintParams[1], data);
+                    } else {
+                        return constraintParams[0](fieldValue, data);
+                    }
+                default:
+                    return new Error();
+            }
+        } else {
+            // handle date value
+            if (Date.parse(fieldValue)) fieldValue = Date.parse(fieldValue);
+
+            // handle others types
+            switch (constraintName) {
+                case 'required':
+                    return !!fieldValue;
+                case 'min':
+                    return fieldValue.length >= constraintParams[0];
+                case 'max':
+                    return fieldValue.length <= constraintParams[0];
+                case 'equalField':
+                    return fieldValue === data[constraintParams[0]];
+                case 'notEqualField':
+                    return fieldValue !== data[constraintParams[0]];
+                case 'equal':
+                    return fieldValue === constraintParams[0];
+                case 'notEqual':
+                    return fieldValue !== constraintParams[0];
+                case 'gt':
+                    return fieldValue > constraintParams[0];
+                case 'gte':
+                    return fieldValue >= constraintParams[0];
+                case 'lt':
+                    return fieldValue < constraintParams[0];
+                case 'lte':
+                    return fieldValue <= constraintParams[0];
+                case 'gtField':
+                    return fieldValue > data[constraintParams[0]];
+                case 'gteField':
+                    return fieldValue >= data[constraintParams[0]];
+                case 'ltField':
+                    return fieldValue < data[constraintParams[0]];
+                case 'lteField':
+                    return fieldValue <= data[constraintParams[0]];
+                case 'in':
+                    return constraintParams[0].includes(fieldValue);
+                case 'notIn':
+                    return !constraintParams[0].includes(fieldValue);
+                case 'pattern':
+                    const patterns = {
+                        email: /^\S+@\S+\.\S+$/,
+                        number: /-?\\d*(\\.\\d+)?$/,
+                        double: /^[0-9]+.[0-9]+$/,
+                        integer: /^[0-9]+/,
+                        alpha: /^[^`~\-!@#$%^&*()_+={}[\]|\\:;“’<,>.?๐฿]*$/
+                    }
+                    if (patterns[constraintParams[0]]) {
+                        if (fieldValue.length === 0) return true;
+                        return patterns[constraintParams[0]].test(fieldValue);
+                    } else {
+                        return false;
+                    }
+                case 'regexp':
+                    return constraintParams[0].test(fieldValue);
+                case 'validator':
+                    return constraintParams(fieldValue, data);
+                case 'validate':
+                    if (constraintParams.length === 3) {
+                        return constraintParams[0](fieldValue, constraintParams[1], data);
+                    } else {
+                        return constraintParams[0](fieldValue, data);
+                    }
+                default:
+                    return new Error();
+            }
+        }
+    }, []);
+
+    const checkConstraints = useCallback((fieldName, constraints, data) => {
+        if (data[fieldName] === '' && !(Object.keys(constraints).includes('required'))) return null;
+        const errors = {};
+        for (const [constraintName, constraintParams] of Object.entries(constraints)) {
+            const isValidated = checkConstraint(constraintName, data[fieldName], constraintParams, data);
+            let message;
+            if (constraintName !== 'validator' && !isValidated) {
+                message = Array.isArray(constraintParams) ? constraintParams[constraintParams.length - 1] : constraintParams;
+            } else if (isValidated && constraintName === 'validator') {
+                message = isValidated;
+            }
+
+            if (message) {
+                if (errors[fieldName]) {
+                    errors[fieldName].push(message);
+                } else {
+                    errors[fieldName] = [message];
+                }
+            }
+        }
+
+        return errors;
+    }, [checkConstraint]);
+
+    const validateField = useCallback((fieldName, data) => {
+        let errors = {};
+        if (schema[fieldName]) {
+            errors = checkConstraints(fieldName, schema[fieldName], data);
+        }
+
+        return errors;
+    }, [schema, checkConstraints]);
+
+    const validateForm = useCallback(data => {
+        let errors = {};
+        for (const [fieldName, constraints] of Object.entries(schema)) {
+            errors = { ...errors, ...checkConstraints(fieldName, constraints, data) };
+        }
+
+        return errors;
+    }, [schema, checkConstraints]);
+
+    const onFieldUpdate = useCallback(e => {
+        const formData = new FormData(ref.current);
+        const formDataObj = Object.fromEntries(formData);
+        const err = validateField(e.target.name, formDataObj);
+        const errs = errors;
+        delete errs[e.target.name];
+
+        setErrors({ ...errs, ...err });
+    }, [errors, validateField]);
+
+    useEffect(() => {
+        const form = ref.current;
+        if (!form) return;
+        const fields = form.querySelectorAll('input, textarea, select');
+        for (const field of fields) {
+            field.addEventListener('change', onFieldUpdate, true);
+            field.addEventListener('keydown', () => setIsValidated(false), true);
+        }
+
+        form.addEventListener('reset', () => {
+            setErrors({});
+            setIsValidated(false);
+        });
+
+        return (() => {
+            for (const field of fields) {
+                field.removeEventListener('change', onFieldUpdate, true);
+                field.removeEventListener('keydown', () => setIsValidated(false), true);
+            }
+
+            form.removeEventListener('reset', () => {
+                setErrors({});
+                setIsValidated(false);
+            });
+        });
+    }, [ref, onFieldUpdate]);
+
+    const reset = useCallback(() => {
+        ref.current.reset();
+    }, []);
+
+    const handleSubmit = useCallback((e, callback) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.target);
+        const formDataObj = Object.fromEntries(formData);
+
+        const err = validateForm(formDataObj);
+        const isErrorsOccured = err && Object.keys(err).length === 0;
+
+        if (isErrorsOccured) callback(formDataObj);
+        setIsValidated(!!isErrorsOccured);
+        setErrors(err);
+    }, [validateForm]);
+
+    const register = useCallback(callback => {
+        return { onSubmit: e => handleSubmit(e, callback), ref }
+    }, [handleSubmit]);
+
+    return { register, errors, setErrors, isValidated, reset };
+}
